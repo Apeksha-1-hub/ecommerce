6344610ba714275ac7063aa5f734f4f6bee0dd21
Apeksha-1-hub <meeraban123@gmail.com>
2025-07-17 11:13:56 +0545

    changes in hero sections

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -4,27 +4,27 @@ import Products from "../product/Product";
 
 const Home = () => {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col py-15">
       {/* Hero Section */}
-      <section className="bg-red-600 text-white flex flex-col md:flex-row items-center justify-center py-20 px-6 text-center md:text-left">
+      <section className="bg-red-600 text-white flex flex-col md:flex-row items-center justify-center py-25 px-6 text-center md:text-left">
         {/* Text Content */}
-        <div className="flex-1 mb-8 md:mb-0">
+        <div className="flex-1 mb-8 md:mb-0 md:pl-20">
           <h1 className="text-4xl sm:text-5xl font-extrabold max-w-3xl leading-tight  mb-10">
             Welcome to ShopEase — Your Ultimate Shopping Destination
           </h1>
-          <p className="max-w-xl mb-8 text-lg sm:text-xl">
+          <p className="max-w-xl mb-8 text-md sm:text-md  ">
             Discover amazing products, unbeatable deals, and fast delivery.
           </p>
           <a
             href="#products"
-            className="bg-amber-400 hover:bg-amber-500 text-black font-semibold px-8 py-3 rounded shadow transition"
+            className="bg-amber-400 hover:bg-black hover:text-white text-black font-semibold px-8 py-3  rounded shadow transition"
           >
             Shop Now
           </a>
         </div>
 
         {/* Image */}
-        <div className="flex-1 flex justify-center">
+        <div className="flex-1 flex justify-center md:pl-40 ">
           <img
             src="photos\girl.png"
             alt="Girl shopping with bags"
